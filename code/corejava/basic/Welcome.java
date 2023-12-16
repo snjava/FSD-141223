4c16afe30221da3b792c50d33bdaf36e3044acf7
snjava <scnaikwadi2@gmail.com>
2023-12-16 15:28:21 +0530

    Git Initial Commit
    Git Initial Commit

---
+++ code/corejava/basic/Welcome.java
@@ -0,0 +1,20 @@
+public class Welcome
+{
+	public static void main(String args[]) 
+	{
+		System.out.println("Welcome To First Java Program");
+		System.out.println(20*3);
+	}
+}
+class SayHello
+{
+	public static void main(String args[]) {
+		System.out.println("Message From SayHello class");
+	}
+}
+class SayBye
+{
+	public static void main(String args[]) {
+		System.out.println("Message From SayBye class");
+	}
+}
