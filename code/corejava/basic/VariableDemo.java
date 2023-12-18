aa87c81020332ca455b03b1422820f8231a6a82e
snjava <scnaikwadi2@gmail.com>
2023-12-18 09:06:01 +0530

    Data Type Intro
    Data Type Intro

---
+++ code/corejava/basic/VariableDemo.java
@@ -0,0 +1,15 @@
+// https://github.com/snjava/FSD-141223
+
+public class VariableDemo
+{
+	public static void main(String args[])
+	{
+		int empId; // declaration 
+		empId = 1234; // initialization
+		double salary = 2342344.23;
+		double monthSal = salary / 12;
+		System.out.println(empId);
+		System.out.println(salary);
+		System.out.println(monthSal);
+	} 
+}
--- code/corejava/basic/Welcome.java
+++ code/corejava/basic/Welcome.java
@@ -1,11 +1,17 @@
+/**
+  This class is to demo the first example for the java program
+  @auther
+  @Since		
+*/
 public class Welcome
 {
 	public static void main(String args[]) 
 	{
 		System.out.println("Welcome To First Java Program");
-		System.out.println(20*3);
+		 System.out.println(20*3);
 	}
 }
+/*
 class SayHello
 {
 	public static void main(String args[]) {
@@ -18,3 +24,4 @@ class SayBye
 		System.out.println("Message From SayBye class");
 	}
 }
+*/
