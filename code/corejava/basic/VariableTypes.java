36e8985a8f3631ab2adf25ceeb3e90ffff1192ba
snjava <scnaikwadi2@gmail.com>
2023-12-20 09:02:32 +0530

    Operator

---
+++ code/corejava/basic/VariableTypes.java
@@ -0,0 +1,13 @@
+public class VariableTypes
+{
+	int b;// instance Variable
+	static int c; // static/class variable 
+
+	public static void main(String ar[])
+	{ 
+		int a = 30;  // Local Variable
+		System.out.println(a);
+		//System.out.println(b);
+		System.out.println(c); // 0
+	}
+}
