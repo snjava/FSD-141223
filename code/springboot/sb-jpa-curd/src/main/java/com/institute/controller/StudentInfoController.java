0cf8d0fcec3f3cd3ef5053d5d76ee3d8e1ef9d13
snjava <scnaikwadi2@gmail.com>
2024-04-16 09:28:36 +0530

    SB JPA CURD Operation
    SB JPA CURD Operation

--- code/springboot/sb-jpa-curd/src/main/java/com/institute/controller/StudentInfoController.java
+++ code/springboot/sb-jpa-curd/src/main/java/com/institute/controller/StudentInfoController.java
@@ -1,8 +1,15 @@
 package com.institute.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.institute.entity.StudentInfo;
@@ -25,7 +32,43 @@ public class StudentInfoController {
 	 */
 	@PostMapping("/save-student-info")
 	public String saveStudent(@RequestBody StudentInfo info) {
-		infoRepository.save(info);
+		infoRepository.save(info); // use to insert or update the record
 		return "Student Details save Successfully...";
 	}
+	
+	// http://localhost:8080/update-student-info
+	@PutMapping("/update-student-info")
+	public String updateStudent(@RequestBody StudentInfo info) {
+		infoRepository.save(info); // use to insert or update the record
+		return "Student Details Updated Successfully...";
+	}
+	
+	// http://localhost:8080/delete-student-by-id?sid=12
+	@DeleteMapping("/delete-student-by-id")
+	public String deleteStudent(@RequestParam("sid") int id) {
+		infoRepository.deleteById(id);
+		return "Student Details Deleted Successfully...";
+	}
+	
+	// http://localhost:8080/get-student-by-id/12
+	@GetMapping("/get-student-by-id/{id}")
+	public StudentInfo getStudentById(@PathVariable(value = "id") int id) {
+		return infoRepository.findById(id).get();
+	}
+	
+	@GetMapping("/get-students")
+	public List<StudentInfo> getAllStudent() {
+		return infoRepository.findAll();
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
