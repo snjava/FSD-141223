56d9bd606c8425c886b1d8d0851390061c923da0
snjava <scnaikwadi2@gmail.com>
2024-04-17 09:24:35 +0530

    SB - Entity Annotations
    SB - Entity Annotations

--- code/springboot/sb-jpa-curd/src/main/java/com/institute/entity/StudentInfo.java
+++ code/springboot/sb-jpa-curd/src/main/java/com/institute/entity/StudentInfo.java
@@ -1,15 +1,62 @@
 package com.institute.entity;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
+@Table(name = "stud_details")
 public class StudentInfo {
+	//@SequenceGenerator(name = "seq_name", initialValue = 1, allocationSize = 1)
+	//@GeneratedValue(generator = "seq_name", strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT functionality of DB
 	@Id
 	private int sid;
+	@Column(name = "stud_name", length = 30, nullable = false)
 	private String name;
+	@Column(unique = true, length = 10)
 	private String contact;
 	private String city;
+	
+	@Transient
+	private int age;
+	
+	@CreationTimestamp
+	@Column(name = "crte_ts")
+	private LocalDateTime createdDateTime;
+	
+	@UpdateTimestamp
+	@Column(name = "updt_ts")
+	private LocalDateTime updatedDateTime;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public LocalDateTime getCreatedDateTime() {
+		return createdDateTime;
+	}
+	public void setCreatedDateTime(LocalDateTime createdDateTime) {
+		this.createdDateTime = createdDateTime;
+	}
+	public LocalDateTime getUpdatedDateTime() {
+		return updatedDateTime;
+	}
+	public void setUpdatedDateTime(LocalDateTime updatedDateTime) {
+		this.updatedDateTime = updatedDateTime;
+	}
 	public int getSid() {
 		return sid;
 	}
